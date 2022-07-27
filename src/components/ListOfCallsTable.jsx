cc66e3ae6a004a01ad1341550a27f549e877d07c
rvyshn <roman.vyshnevetskyi@solarwinds.com>
2022-07-27 15:27:49 +0300

    add Test Task

--- src/components/ListOfCallsTable.jsx
+++ src/components/ListOfCallsTable.jsx
@@ -2,24 +2,15 @@ import React from 'react';
 import {Table} from "antd";
 
 const ListOfCallsTable = ({data}) => {
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'name',
-        },
-        {
-            title: 'Status',
-            dataIndex: 'status',
-        },
-        {
-            title: 'Date',
-            dataIndex: 'date',
-        },
-    ];
+    const columns = [{
+        title: 'Name', dataIndex: 'name',
+    }, {
+        title: 'Status', dataIndex: 'status',
+    }, {
+            title: 'Date', dataIndex: 'date',
+        },];
 
-    return (
-        <Table dataSource={data} columns={columns}  pagination={false}/>
-    );
+    return (<Table dataSource={data} columns={columns} pagination={false}/>);
 };
 
 export default ListOfCallsTable;
